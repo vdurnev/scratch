6ad4b84d831af4aa2c1fbbf13e8d5f6d4592ba59
Vitaliy Durnev <vdurnev@gmail.com>
2020-06-17 09:44:40 +0300

    New version of file

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo yes | mv /Users/vdurnev/Downloads/Танки.html /Users/vdurnev/Sources/Scratch/AndroidApp/www/index.html
 git status
-git add -A
-git commit -m "New version of file"
+git commit -am "New version of file"
 git push
